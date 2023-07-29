3f304fc66f60257630c120ed11421e5dbe084997
Melisa <melisahrnn@gmail.com>
2023-07-28 23:21:35 -0400

    Login.java modfied; Rules.java created.

--- src/quiz_application/Login.java
+++ src/quiz_application/Login.java
@@ -6,6 +6,7 @@ import java.awt.event.*;
 public class Login extends JFrame implements ActionListener {
     //Global variables
     JButton rulesBtn, backBtn;
+    JTextField inputName;
     Login(){
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
@@ -15,6 +16,7 @@ public class Login extends JFrame implements ActionListener {
         JLabel image = new JLabel(loginIcon);
         image.setBounds(0,0,600,500);
         add(image);
+
         //Tile,name, and text field
         JLabel title = new JLabel("Simple Minds");
         title.setBounds(750,60,300,45);
@@ -25,13 +27,14 @@ public class Login extends JFrame implements ActionListener {
         JLabel name = new JLabel("Enter your name");
         name.setBounds(810,150,300,20);
         name.setFont(new Font("Serif", Font.BOLD,18));
-        name.setForeground(new Color(30,144,254));//Font color
+        name.setForeground(new Color(30,144,254));
         add(name);
 
-        JTextField inputName = new JTextField();
+        inputName = new JTextField();
         inputName.setBounds(735,200,300,25);
         inputName.setFont(new Font("Times New Roman", Font.BOLD, 20));
         add(inputName);
+
         //Button(rules + back) with an action listener
         rulesBtn = new JButton("Rules");
         rulesBtn.setBounds(735,270,120,25);
@@ -59,7 +62,9 @@ public class Login extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == rulesBtn){
-
+            String name = inputName.getText();
+            setVisible(false);
+            new Rules(name);
         } else if (e.getSource() == backBtn){
             setVisible(false);
         }
---
+++ src/quiz_application/Rules.java
@@ -0,0 +1,72 @@
+package quiz_application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Rules extends JFrame implements ActionListener {
+    String name;
+    JButton startBtn, backBtn;
+    Rules(String name){
+        this.name = name;
+        getContentPane().setBackground(Color.white);
+        setLayout(null);
+
+        JLabel title = new JLabel("Welcome "+ name +" to Simple Minds");
+        title.setBounds(50,20,700,30);
+        title.setFont(new Font("Tahoma", Font.BOLD,28));
+        title.setForeground(new Color(30,144,254));
+        add(title);
+
+        JLabel rules = new JLabel();
+        rules.setBounds(20,90,700,350);
+        rules.setFont(new Font("Tahoma", Font.PLAIN,16));
+        rules.setText(
+              "<html>"+
+                    "1. Programming can be frustrating, but keep trying."+ "<br><br>" +
+                    "2. Keep your code simple and readable." + "<br><br>" +
+                    "3. Build projects that you'll enjoy." + "<br><br>" +
+                    "4. Practice, practice, practice!" + "<br><br>" +
+                    "5. Technology is always evolving." + "<br><br>" +
+                    "6. Its a lifelong learning commitment." + "<br><br>" +
+                    "7. Good Luck! :D"+ "<br><br>" +
+              "<html>"
+        );
+        add(rules);
+
+        backBtn = new JButton("Back");
+        backBtn.setBounds(250,500,100,30);
+        backBtn.setBackground(new Color(30,144,254));
+        backBtn.setForeground(Color.white);
+        backBtn.addActionListener(this);
+        add(backBtn);
+
+        startBtn = new JButton("Start");
+        startBtn.setBounds(400,500,100,30);
+        startBtn.setBackground(new Color(30,144,254));
+        startBtn.setForeground(Color.white);
+        startBtn.addActionListener(this);
+        add(startBtn);
+
+        setSize(800,650);
+        setLocation(350,100);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Rules("User");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == startBtn){
+
+        }else{
+            setVisible(false);
+            new Login();
+        }
+    }
+}
+
+
+
